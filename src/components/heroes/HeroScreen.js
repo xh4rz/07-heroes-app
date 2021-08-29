34d3b6b711dbbd90948264e21703fd78bbf28b52
Harold Gonzalez <xh4rz@hotmail.com>
2021-08-29 18:15:12 -0500

    10._Leer_argumentos_por_URL

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Redirect, useParams } from 'react-router';
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
-    return (
-        <div>
-            <h1>HeroScreen</h1>
-        </div>
-    )
-}
+    
+	const { heroeId } = useParams();
+
+	const hero = getHeroById(heroeId);
+
+	if (!hero) {
+		return <Redirect to="/" />;
+	}
+
+	const { superhero, publisher, alter_ego, first_appearance, characters } =
+		hero;
+
+	// console.log(hero)
+
+	return (
+		<div>
+			<h1>HeroScreen</h1>
+		</div>
+	);
+};
