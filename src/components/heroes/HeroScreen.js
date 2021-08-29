4a7d3cd1a7c90ad7029e216fbf12bb4aa7fc79e6
Harold Gonzalez <xh4rz@hotmail.com>
2021-08-29 18:34:08 -0500

    11._Estilo_del_componente_HeroScreen

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router';
 import { getHeroById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
-    
+export const HeroScreen = ({ history }) => {
 	const { heroeId } = useParams();
 
 	const hero = getHeroById(heroeId);
@@ -15,11 +14,42 @@ export const HeroScreen = () => {
 	const { superhero, publisher, alter_ego, first_appearance, characters } =
 		hero;
 
-	// console.log(hero)
+	const handleReturn = () => {
+		if (history.length <= 2) {
+			history.push('/');
+		} else {
+			history.goBack();
+		}
+	};
 
 	return (
-		<div>
-			<h1>HeroScreen</h1>
+		<div className="row mt-5">
+			<div className="col-4">
+				<img
+					src={`../assets/heroes/${heroeId}.jpg`}
+					alt={superhero}
+					className="img-thumbnail"
+				/>
+			</div>
+			<div className="col-8">
+				<h3>{superhero}</h3>
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">
+						<b>Alter ego:</b> {alter_ego}
+					</li>
+					<li className="list-group-item">
+						<b>Publisher:</b> {publisher}
+					</li>
+					<li className="list-group-item">
+						<b>First appearance:</b> {first_appearance}
+					</li>
+				</ul>
+				<h5>characters</h5>
+				<p>{characters}</p>
+				<button className="btn btn-outline-info" onClick={handleReturn}>
+					Return
+				</button>
+			</div>
 		</div>
 	);
 };
