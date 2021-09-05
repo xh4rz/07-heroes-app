ff48997e864d0ad8057eff092cc56f9ae5443917
Harold Gonzalez <xh4rz@hotmail.com>
2021-09-05 16:49:48 -0500

    7._Probar_que_el_PrivateRoute_no_muestre_el_componente

--- src/tests/routers/PrivateRoute.test.js
+++ src/tests/routers/PrivateRoute.test.js
@@ -26,4 +26,19 @@ describe('Pruebas en <PrivateRoute />', () => {
 		expect(wrapper.find('span').exists()).toBe(true);
 		expect(localStorage.setItem).toHaveBeenCalledWith('lastPath', '/marvel');
 	});
+
+	test('debe de bloquear el componente si no está autenticado', () => {
+		const wrapper = mount(
+			<MemoryRouter>
+				<PrivateRoute
+					isAuthenticated={false}
+					component={() => <span>Listo!</span>}
+					{...props}
+				/>
+			</MemoryRouter>
+		);
+
+		expect(wrapper.find('span').exists()).toBe(false);
+		expect(localStorage.setItem).toHaveBeenCalledWith('lastPath', '/marvel');
+	});
 });
