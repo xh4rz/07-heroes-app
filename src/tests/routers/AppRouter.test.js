c0c5de0b1903159dafe048e6907ac657d7978358
Harold Gonzalez <xh4rz@hotmail.com>
2021-09-05 22:59:06 -0500

    8._Pruebas_en_el_componente_AppRouter

---
+++ src/tests/routers/AppRouter.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { AppRouter } from '../../routers/AppRouter';
+import { AuthContext } from '../../auth/AuthContext';
+
+describe('Pruebas en <AppRouter />', () => {
+	const contextValue = {
+		dispatch: jest.fn(),
+		user: {
+			logged: false
+		}
+	};
+
+	test('debe de mostrar login si no está autenticado', () => {
+		const wrapper = mount(
+			<AuthContext.Provider value={contextValue}>
+				<AppRouter />
+			</AuthContext.Provider>
+		);
+
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	test('debe de mostrar el component marvel si está autenticado', () => {
+		const contextValue = {
+			dispatch: jest.fn(),
+			user: {
+				logged: true,
+				name: 'Harold'
+			}
+		};
+
+		const wrapper = mount(
+			<AuthContext.Provider value={contextValue}>
+				<AppRouter />
+			</AuthContext.Provider>
+		);
+		expect(wrapper.find('.navbar').exists()).toBe(true);
+	});
+});
