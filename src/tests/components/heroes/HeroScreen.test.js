71615aaf9529b93e4118eaa469985620798b59f5
Harold Gonzalez <xh4rz@hotmail.com>
2021-10-03 21:13:03 -0500

    11._Pruebas_en_el_componente_HeroScreen

---
+++ src/tests/components/heroes/HeroScreen.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { HeroScreen } from '../../../components/heroes/HeroScreen';
+import { MemoryRouter } from 'react-router';
+
+describe('Pruebas en <HeroScreen />', () => {
+	const history = {
+		length: 10,
+		push: jest.fn(),
+		goBack: jest.fn()
+	};
+
+	const wrapper = mount(
+		<MemoryRouter initialEntries={['/hero']}>
+			<HeroScreen history={history} />
+		</MemoryRouter>
+	);
+
+	test('debe de de mostrar el componente redirect si no hay argumentos en el URL', () => {
+		expect(wrapper.find('Redirect').exists()).toBe(true);
+	});
+});
